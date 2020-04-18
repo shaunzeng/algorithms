b2ef344232cdc9660929a5f087bffdd8bf46266c
Shaun Zeng <zengxiangxiang@gmail.com>
2020-04-18 18:53:45 -0400

    re structure

--- src/sorting_algorithm.js
+++ src/sorting_algorithm.js
@@ -2,19 +2,19 @@
 export function bubbleSort(arr, ascending = true) {
     let len = arr.length;
 
-    for (let i = 0; i < len-1; i++) {
-        for (let j = 0 ; j < len - 1 - i; j++) {
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
             if (!!ascending) {
-                if (arr[j] > arr[j+1]){
+                if (arr[j] > arr[j + 1]) {
                     let temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             } else {
-                if (arr[j] < arr[j+1]) {
+                if (arr[j] < arr[j + 1]) {
                     let temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
@@ -25,58 +25,58 @@ export function bubbleSort(arr, ascending = true) {
 
 export function recursiveBubbleSort(arr, len = arr.length, ascending = true) {
 
-    if (len == 1){
-        return ;
+    if (len == 1) {
+        return;
     }
 
-    for (let i = 0; i < len - 1; i++){
-        if (!!ascending){
-            if (arr[i] > arr[i+1]){
+    for (let i = 0; i < len - 1; i++) {
+        if (!!ascending) {
+            if (arr[i] > arr[i + 1]) {
                 let temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
             }
         } else {
-            if (arr[i] < arr[i+1]){
+            if (arr[i] < arr[i + 1]) {
                 let temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
             }
         }
     }
 
-    recursiveBubbleSort(arr, len-1, ascending);
+    recursiveBubbleSort(arr, len - 1, ascending);
     return arr;
 
 }
 
 // selection sort
-export function selectionSort(arr, ascending){
+export function selectionSort(arr, ascending) {
     let len = arr.length,
         minIdx;
-    
-        for (let i = 0; i < len ; i++){
-            minIdx = i;
 
-            for (let j = i + 1; j < len; j++ ){
-                if (!!ascending){
-                    if (arr[j] < arr[minIdx]){
-                        minIdx = j;
-                    }
-                } else {
-                    if (arr[j] > arr[minIdx]){
-                        minIdx = j;
-                    }
+    for (let i = 0; i < len; i++) {
+        minIdx = i;
+
+        for (let j = i + 1; j < len; j++) {
+            if (!!ascending) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
                 }
-                
+            } else {
+                if (arr[j] > arr[minIdx]) {
+                    minIdx = j;
+                }
             }
 
-            if (minIdx !== i){
-                let temp = arr[i];
-                arr[i] = arr[minIdx];
-                arr[minIdx] = temp;
-            }
         }
+
+        if (minIdx !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
 
     return arr;
 }
@@ -84,14 +84,14 @@ export function selectionSort(arr, ascending){
 // insertion sort
 export function insertionSort(arr) {
     let len = arr.length,
-        i,j,temp;
-    
-    for(i = 1; i < len; i++) {
+        i, j, temp;
+
+    for (i = 1; i < len; i++) {
         j = i;
         temp = arr[i];
-        while(j>0 && arr[j-1]>temp){
-            arr[j] = arr[j-1];
-            j --;
+        while (j > 0 && arr[j - 1] > temp) {
+            arr[j] = arr[j - 1];
+            j--;
         }
         arr[j] = temp;
     }
@@ -101,26 +101,26 @@ export function insertionSort(arr) {
 
 
 export function mergeSort(arr) {
-    if (arr.length < 2){
+    if (arr.length < 2) {
         return arr;
     }
 
-    let mid = Math.floor(arr.length/2),
+    let mid = Math.floor(arr.length / 2),
         left = arr.slice(0, mid),
         right = arr.slice(mid, arr.length);
 
     const merge = (left, right) => {
         let output = [];
 
-        while(left.length !== 0 && right.length !== 0 ) {
+        while (left.length !== 0 && right.length !== 0) {
             output.push(left[0] < right[0] ? left.shift() : right.shift());
         }
 
-        while(left.length !== 0) {
+        while (left.length !== 0) {
             output.push(left.shift());
         }
 
-        while(right.length !== 0 ){
+        while (right.length !== 0) {
             output.push(right.shift());
         }
 
@@ -128,35 +128,35 @@ export function mergeSort(arr) {
     }
 
     return merge(mergeSort(left), mergeSort(right));
-    
+
 }
 
-var Stack = function(){
+var Stack = function() {
     var self = this;
     self.data = [];
     self.push = push;
     self.pop = pop;
     self.peek = peek;
     self.size = size;
-    
-    function push(val){
+
+    function push(val) {
         self.data.push(val);
     }
-    
+
     function pop() {
         return self.data.pop();
     }
-    
-    function peek(){
+
+    function peek() {
         return self.data[self.data.length - 1];
     }
-    
-    function size(){
+
+    function size() {
         return self.data.length;
     }
-} 
+}
 
-function GraphNode(val){
+function GraphNode(val) {
     this.val = val;
     this.neighbors = [];
     this.visited = false;
@@ -170,10 +170,10 @@ function DFS(starting_node) {
 
     s.push(starting_node);
 
-    while(s.size() != 0) {
+    while (s.size() != 0) {
         var current = s.pop();
 
-        if (!current.visited){
+        if (!current.visited) {
             current.visited = true;
 
             console.log('visiting node : ', current.val);
@@ -182,14 +182,14 @@ function DFS(starting_node) {
                 if (!current.neighbors[i].visited) {
                     s.push(current.neighbors[i]);
                 }
-                
+
             }
         }
     }
 }
 
 var A = new GraphNode('A');
-A.player ='one';
+A.player = 'one';
 var B = new GraphNode('B');
 var C = new GraphNode('C');
 var D = new GraphNode('D');
@@ -202,29 +202,29 @@ var H = new GraphNode('H');
 var I = new GraphNode('I');
 
 
-A.neighbors = [B,D,E];
-B.neighbors = [A,D,E,F,C];
-C.neighbors = [B,E,F];
-D.neighbors = [A,B,E,H,G];
-E.neighbors = [A,B,C,D,F,G,H,I];
-F.neighbors = [C,B,E,H,I];
-G.neighbors = [D,E,H];
-H.neighbors = [G,D,E,F,I];
-I.neighbors = [E,F,H];
+A.neighbors = [B, D, E];
+B.neighbors = [A, D, E, F, C];
+C.neighbors = [B, E, F];
+D.neighbors = [A, B, E, H, G];
+E.neighbors = [A, B, C, D, F, G, H, I];
+F.neighbors = [C, B, E, H, I];
+G.neighbors = [D, E, H];
+H.neighbors = [G, D, E, F, I];
+I.neighbors = [E, F, H];
 
 DFS(A);
 
 
 
 // heapSort using Min Int Heap 
-export function heapSort(arr){
+export function heapSort(arr) {
     if (arr.length <= 1) return arr;
 
-    
+
     const buildMinHeap = () => {
         let len = arr.length - 1;
 
-        for (let i = len ; i>0; i--){
+        for (let i = len; i > 0; i--) {
             heapifyUp(i);
         }
     }
@@ -236,7 +236,7 @@ export function heapSort(arr){
     }
 
     const hasParent = (index) => {
-        return index/2 > 0;
+        return index / 2 > 0;
     }
 
     const hasLeftChild = (parentIndex) => {
@@ -272,7 +272,7 @@ export function heapSort(arr){
     }
 
     const heapifyUp = (index) => {
-        while(hasParent(index) && arr[index] < parent(arr,index)){
+        while (hasParent(index) && arr[index] < parent(arr, index)) {
             swap(index, getParentIndex(index));
             index = getParentIndex(index);
         }
@@ -282,10 +282,10 @@ export function heapSort(arr){
         if (arr.length < 2) return arr;
         arr.unshift(arr.pop());
         let index = 0;
-        while(hasLeftChild(index)) {
+        while (hasLeftChild(index)) {
             let smallerChildIndex = getLeftChildIndex(index);
 
-            if (hasRightChild(index) && rightChild(index) < leftChild(index) ) {
+            if (hasRightChild(index) && rightChild(index) < leftChild(index)) {
                 smallerChildIndex = getRightChildIndex(index);
             }
 
@@ -300,9 +300,9 @@ export function heapSort(arr){
     }
 
     buildMinHeap(arr);
- 
+
     let output = [];
-    while(arr.length > 0) {
+    while (arr.length > 0) {
         output.push(arr.shift());
         heapifyDown(arr);
     }
@@ -312,5 +312,9 @@ export function heapSort(arr){
 
 
 export function radixSort(arr) {
-    
+
+}
+
+export function quickSort(A, l, r) {
+
 }
