1b2136a2127ecb97245fd515264355832a4dc952
Shaun Zeng <zengxiangxiang@gmail.com>
2021-01-04 22:53:09 -0500

    more algos

--- src/leetcode/210.CourseSchedule2.js
+++ src/leetcode/210.CourseSchedule2.js
@@ -4,60 +4,59 @@
  * @return {number[]}
  */
 
-/*  topological sort
-Classic topological sort, but watch for scenario where there is a cycle.
+// cyrcle detection in directed graph,
 
-*/
+// three sets: graph, visited, visiting
+
+// if visiting has duplicate , there s a cycle, return [];
+// remove visiting node once it reaches the end, and add end node to visited in post traversal, while adding the end
+// node to the ans array / tracking the path;
 var findOrder = function(numCourses, prerequisites) {
+    if (numCourses <= 1) return !!prerequisites[0] ? prerequisites[0].reverse() : [0];
 
     const graph = buildGraph(numCourses, prerequisites),
-        visited = new Map(), // (undefined = unknown, 1 = visiting, 2 = visited)
-        s = [];
+        visiting = new Set(),
+        visited = new Set(),
+        ans = [];
 
     for (let [course, neighbors] of Object.entries(graph)) {
-        // if any returns true, that means there s a cycle, return empty array
-        if (dfs(course, s, visited, graph)) return [];
-    };
+        if (hasCycle(course, graph, visiting, visited, ans)) return [];
+    }
 
-    return s;
+    return ans;
 };
 
-function dfs(course, s, visited, graph) {
 
-    // if course is 1 in visited, that means it has been already visiting, there is a cyrcle
-    if (visited.get(course) === 1) return true;
+function hasCycle(course, graph, visiting, visited, ans) {
 
-    // if course is 2 in visited, that means it has already visited, no need to visit again 
-    if (visited.get(course) === 2) return false;
+    if (visiting.has(course)) return true;
+    if (visited.has(course)) return false;
 
-    visited.set(course, 1);
+    visiting.add(course);
 
     const neighbors = graph[course];
 
     for (let i = 0; i < neighbors.length; i++) {
-        if (dfs(neighbors[i], s, visited, graph)) return true;
+        if (visited.has(neighbors[i])) continue;
+        if (hasCycle(neighbors[i], graph, visiting, visited, ans)) return true;
     }
 
-    // set visiting to visited
-    visited.set(course, 2);
-
-    // push 
-    s.unshift(course);
+    visiting.delete(course);
+    visited.add(course);
+    ans.unshift(course);
     return false;
 }
 
 
 function buildGraph(num, arr) {
-    let graph = {};
+    const graph = {};
 
     for (let i = 0; i < num; i++) {
         graph[i] = [];
     }
 
     arr.forEach(edge => {
-        let pre = edge[1],
-            course = edge[0];
-        graph[pre].push(course.toString());
+        graph[edge[1]].push(edge[0] + ''); // convert to string
     });
 
     return graph;
